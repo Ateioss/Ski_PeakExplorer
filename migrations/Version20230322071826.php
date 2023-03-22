79bb966b86ab3466e31859f439235f64421cee4c
Nalithdev <Ethan.delcroix@outlook.fr>
2023-03-22 08:49:06 +0100

    Add Crud Easyadmin more data in User entity

--- migrations/Version20230322071826.php
+++ migrations/Version20230322071826.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230320232334 extends AbstractMigration
+final class Version20230322071826 extends AbstractMigration
 {
     public function getDescription(): string
     {
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher){}
+
+    public function load(ObjectManager $manager): void
+    {
+        //Créons un utilisateur
+        $user = new User();
+        $user->setEmail('francisbertrand@gmail.com');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+        $user->setFirstname('Francis');
+        $user->setLastname('Bertrand');
+
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
+
+        $manager->flush();
+
+        //Créons un admin
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'password'));
+        $admin->setFirstname('Admin');
+        $admin->setLastname('Admin');
+
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $manager->flush();
+
+        //Créons un commerçant
+
+        $Station = new User();
+        $Station->setEmail('philippe.lafont@gmail.com');
+        $Station->setPassword($this->passwordHasher->hashPassword($Station, 'password'));
+        $Station->setFirstname('Philippe');
+        $Station->setLastname('Lafont');
+
+        $Station->setRoles(['ROLE_STATION']);
+
+        $manager->persist($Station);
+
+        $manager->flush();
+
+        $manager->flush();
+    }
+}
