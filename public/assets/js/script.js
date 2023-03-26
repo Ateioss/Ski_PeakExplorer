87a02d480985cc8a81588db2a46bef2f92575401
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-03-26 16:27:39 +0200

    :lipstick:animation tentative 1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Félin", " Solitaire", "Discret"],
+        strings: ["Service", " à l'écoute", "sérieux"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
